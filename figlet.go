dba04dc7959946d1b185346e04868b7727ddf786
bep <8226605+bepvte@users.noreply.github.com>
2018-05-30 12:39:55 -0400

    hooty

--- figlet.go
+++ figlet.go
@@ -9,16 +9,17 @@ import (
 )
 
 func figlet(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if len(strings.Replace(m.Content, " ", "", -1)) > 10 || len(m.Content) == 0 {
+	trimmed := strings.Join(strings.Split(m.Content, " ")[1:], " ")
+	if len(strings.Replace(trimmed, " ", "", -1)) > 10 || len(trimmed) == 0 {
 		s.ChannelMessageSend(m.ChannelID, "Usage: "+prefix+"figlet [less then 10 letters]")
 		return
 	}
-	c := exec.Command("figlet", m.Content)
+	c := exec.Command("figlet", trimmed)
 	var out bytes.Buffer
 	c.Stdout = &out
 	if err := c.Run(); err != nil {
 		log.Printf("[FIGLET] Error: %v\n", err)
 		return
 	}
-	s.ChannelMessageSend(m.ChannelID, "```\n"+strings.Replace(out.String(), "`", "\\`", -1)+"\n```")
+	s.ChannelMessageSend(m.ChannelID, "```\n"+out.String()+"\n```")
 }
